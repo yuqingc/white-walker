0468967b343c67c9b534922d5fa14b993ea82208
Matt <mr.chenyuqing@live.com>
2018-03-26 22:28:31 +0800

    add testide

---
+++ test/testide/testide.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("VSCode debug tool works!")
+}
+
+/*
+How to install the dev tool
+go env GOPATH
+git clone https://github.com/derekparker/delve.git ~/go/src/github.com/derekparker/delve
+cd ~/go/src/github.com/derekparker/delve/
+make install
+*/
