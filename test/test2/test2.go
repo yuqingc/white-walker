6e6eefb965b0a2ce0c7c07df7164c20d4ad299a9
Matt <mr.chenyuqing@live.com>
2018-03-26 22:17:04 +0800

    http go

--- test/test2/test2.go
+++ test/test2/test2.go
@@ -2,8 +2,39 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
 )
 
 func main() {
 	fmt.Println("This is test2")
+	mux := http.NewServeMux()
+	mux.Handle("/", &myHandler{})
+	mux.HandleFunc("/hello", sayHello)
+
+	wd, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	mux.Handle("/static/",
+		http.StripPrefix("/static/",
+			http.FileServer(http.Dir(wd))))
+
+	err = http.ListenAndServe(":8080", mux)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+type myHandler struct{}
+
+func (h *myHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "URL:"+r.URL.String())
+}
+
+func sayHello(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "Winter is coming v1")
 }
