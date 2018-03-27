6da3cf77ae38d70b15cf63f344277a49aaad4022
Yuqing Chen <chenyuqing@caicloud.io>
2018-03-27 09:29:16 +0800

    add a msg that I should add the gopath/bin to the os path

--- test/testide/testide.go
+++ test/testide/testide.go
@@ -14,4 +14,5 @@ go env GOPATH
 git clone https://github.com/derekparker/delve.git ~/go/src/github.com/derekparker/delve
 cd ~/go/src/github.com/derekparker/delve/
 make install
+add $GOPATH/bin to PATH
 */
